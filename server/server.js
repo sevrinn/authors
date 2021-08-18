4c4c88d773935ccb8213d29427b40a4d0cde199d
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-18 15:29:38 -0700

    added comments to server js so i dont forget anything

--- server/server.js
+++ server/server.js
@@ -1,5 +1,17 @@
+//import express and other libraries
 const express = require("express");
 const app = express();
 const port = 8000;
 
+//add in cors
+
+//configure express app server
+
+//configure cors to talk directly to back end
+
+//configure mongoose to connect
+
+//add routes to listen
+
+// start the app server listening
 app.listen(port, () => console.log(`Listening on port ${8000}`));
