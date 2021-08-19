219ac035344f6c4fa5c710667ce08b096963e842
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-19 07:14:36 -0700

    authors print to page in a table with their own edit and delete buttons. Edit button goes to that authors details page.

--- client/src/components/AllAuthors.js
+++ client/src/components/AllAuthors.js
@@ -1,11 +1,51 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, navigate } from "@reach/router";
+import Header from "./Header";
 
 const AllAuthors = (props) => {
+  const [allAuthors, setAllAuthors] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/authors/")
+      .then((res) => {
+        console.log(res);
+        setAllAuthors(res.data);
+      })
+      .catch((err) => {
+        console.log("there was an error", err);
+      });
+  }, []);
+
   return (
     <div>
-      <h2>All Authors</h2>
+      <Link to={"/authors/new"}>Add an author</Link>
+      <p>We have quotes by:</p>
+      <table>
+        <tbody>
+          <tr>
+            <th>Author</th>
+            <th>Actions available</th>
+          </tr>
+          {allAuthors.map((author, idx) => {
+            console.log(author.name);
+
+            return (
+              <tr key={idx}>
+                <td>{author.name}</td>
+                <td>
+                  <Link to={"/authors/" + author._id}>
+                    <button>Edit</button>
+                  </Link>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {/* <h2>All Authors</h2> */}
     </div>
   );
 };
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, navigate } from "@reach/router";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>Favorite authors</h1>
+    </div>
+  );
+};
+
+export default Header;
